1e40b5ecd111e6ce05b3927d2b615faf22821d51
gleb0375 <82374352+gleb0375@users.noreply.github.com>
2025-08-29 22:04:13 +0200

    creating contact service

---
+++ api-contact/src/main/java/com/hhnatsiuk/api_contact/ApiContactApplication.java
@@ -0,0 +1,13 @@
+package com.hhnatsiuk.api_contact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiContactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiContactApplication.class, args);
+    }
+
+}
---
+++ api-contact/src/test/java/com/hhnatsiuk/api_contact/ApiContactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hhnatsiuk.api_contact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiContactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
